0a9b69f1b7657eddc96a13517baff6c8c717968f
iRama7 <ricoramiro552@gmail.com>
2024-09-11 18:39:36 -0300

    Fixed Custom model data support.

--- src/main/java/rama/ir/NBTMain.java
+++ src/main/java/rama/ir/NBTMain.java
@@ -1,8 +1,14 @@
 package rama.ir;
 
+import de.tr7zw.changeme.nbtapi.NBT;
 import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
+import de.tr7zw.changeme.nbtapi.iface.ReadableItemNBT;
+import de.tr7zw.changeme.nbtapi.utils.MinecraftVersion;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.function.Function;
 
 public class NBTMain {
 
@@ -27,14 +33,31 @@ public class NBTMain {
     }
 
     public int getCustomModelData(ItemStack item){
-        NBTItem nbtItem = new NBTItem(item);
-        Bukkit.getLogger().info("Custom model data for item " + item.getType() + " is " + nbtItem.getInteger("CustomModelData"));
-        return nbtItem.getInteger("CustomModelData");
+        int customModelData = 0;
+        if(MinecraftVersion.isAtLeastVersion(MinecraftVersion.MC1_20_R4)){
+            customModelData = NBT.modifyComponents(item, (Function<ReadWriteNBT, Integer>) nbt ->
+                    nbt.getInteger("minecraft:custom_model_data"));
+        }else{
+            customModelData = NBT.get(item, (Function<ReadableItemNBT, Integer>) nbt ->
+                    nbt.getInteger("CustomModelData"));
+        }
+
+        Bukkit.getLogger().info("Custom model data for item " + item.getType() + " is " + customModelData);
+
+        return customModelData;
     }
 
     public boolean hasCustomModelData(ItemStack item){
-        NBTItem nbtItem = new NBTItem(item);
-        return nbtItem.getInteger("CustomModelData") != 0;
+        int customModelData = 0;
+        if(MinecraftVersion.isAtLeastVersion(MinecraftVersion.MC1_20_R4)){
+            customModelData = NBT.modifyComponents(item, (Function<ReadWriteNBT, Integer>) nbt ->
+                    nbt.getInteger("minecraft:custom_model_data"));
+        }else{
+            customModelData = NBT.get(item, (Function<ReadableItemNBT, Integer>) nbt ->
+                    nbt.getInteger("CustomModelData"));
+        }
+
+        return customModelData != 0;
     }
 
 }
